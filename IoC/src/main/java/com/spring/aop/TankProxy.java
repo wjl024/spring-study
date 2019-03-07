357e603b069c818246be3e9a92f8342ab63f1390
wjl024 <1433686098@qq.com>
2019-03-07 10:56:40 +0800

    aop practise

---
+++ IoC/src/main/java/com/spring/aop/TankProxy.java
@@ -0,0 +1,16 @@
+package com.spring.aop;
+
+public class TankProxy implements Move {
+    private Move t;
+
+    public TankProxy(Move t) {
+        this.t = t;
+    }
+
+    @Override
+    public void move() {
+        System.out.println("start");
+        t.move();
+        System.out.println("stop");
+    }
+}
